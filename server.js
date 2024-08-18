aece85f075b9d6b6f6cc2dac0da1288aa631ef2c
Dheeraj Pal <dheerajpal001@gmail.com>
2024-08-18 08:17:41 +0530

    first commit

--- server.js
+++ server.js
@@ -3,33 +3,29 @@ const app = express()
 const http = require('http')
 const Server = http.createServer(app)
 const socketTo = require('socket.io')
+const dotenv = require('dotenv');
 
-
+dotenv.config();
 app.use(express.static(__dirname + '/public'))
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
 // Socket 
 // const io = require('socket.io')(http)
-const io = socketTo(Server , {
-  cors:{
-    origin:`*`,
-    methods: ['GET','POST']
-  }
-})
+const io = socketTo(Server)
 
 io.on('connection', (socket) => {
   console.log('Connected...')
   socket.on('message', (msg) => {
     socket.broadcast.emit('message', msg)
   })
-
+  socket.on("disconnect", (data) => {
+    socket.broadcast.emit('disconnect', data);
+  });
 })
-const PORT = process.env.PORT || 3000
 
-Server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
+Server.listen(process.env.PORT, () => {
+    console.log(`Listening on port http://localhost:${process.env.PORT}`)
 })
 
