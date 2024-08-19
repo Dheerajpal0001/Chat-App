462ce2d76cacd5297d5135c5109dadc4db42c20b
Dheeraj Pal <dheerajpal001@gmail.com>
2024-08-19 08:33:02 +0530

    commit

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-const express = require('express')
-const app = express()
-const http = require('http')
+const express = require('express');
+const http = require('http');
 const path = require('path');
-const Server = http.createServer(app)
-const socketTo = require('socket.io')
+const socketIo = require('socket.io');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
 dotenv.config();
-app.use(express.static(path.join(__dirname + '/public')));
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
-app.use(cors());
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-// Socket 
-// const io = require('socket.io')(http)
-const io = socketTo(Server)
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 io.on('connection', (socket) => {
-  // console.log('Connected...')
-  socket.on('message', (msg) => {
-    socket.broadcast.emit('message', msg)
-  })
-})
+  console.log('A user connected');
 
-Server.listen(process.env.PORT || 3000, () => {
-    console.log(`Listening on port http://localhost:${process.env.PORT}`)
-})
+  socket.on('message', (msg) => {
+    socket.broadcast.emit('message', msg);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
